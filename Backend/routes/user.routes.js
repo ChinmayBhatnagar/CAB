476a5fb79faa2d9fe55b3fabc26cc02c4b258a0e
ChinmayBhatnagar <142470064+ChinmayBhatnagar@users.noreply.github.com>
2024-12-22 21:53:07 +0530

    user authentication done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const {body} = require("express-validator")
+const { body } = require("express-validator")
 const userController = require('../controllers/user.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
+
 
 router.post('/register', [
     body('email').isEmail().withMessage('invalid email'),
-    body('fullname.firstname').isLength({min : 3}).withMessage('first name must be atleast 3 char'),
-    body('password').isLength({min: 6}).withMessage('must be 6')
+    body('fullname.firstname').isLength({ min: 3 }).withMessage('first name must be atleast 3 char'),
+    body('password').isLength({ min: 6 }).withMessage('must be 6')
 
 ],
-userController.registerUser
+    userController.registerUser
 )
+router.post('/login',[
+    body('email').isEmail().withMessage('invalid email'),
+    body('password').isLength({ min: 6 }).withMessage('password')
+], 
+    userController.loginUser    
+)
+
+router.get('/profile',authMiddleware.authUser, userController.getUserProfile)
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 
 module.exports = router;
