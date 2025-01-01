bbfa870d599691ec513e542b686d34334f147701
ChinmayBhatnagar <142470064+ChinmayBhatnagar@users.noreply.github.com>
2025-01-01 23:51:42 +0530

    updated on 1-1-2025 11.51pm

---
+++ frontend/src/components/RidePopUp.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const RidePopUp = (props) => {
+    return (
+        <div>
+            <h5
+                className="p-1 text-center w-[93%] absolute top-0 "
+                onClick={() => {
+                    props.setRidePopUpPanel(false)
+                }}
+            >
+                <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
+            </h5>
+            <h3 className="text-2xl font-semibold mb-5">Ride Available!</h3>
+
+            <div className='flex items-center justify-between mt-4 p-5 bg-yellow-400 rounded-lg'>
+                <div className='flex items-center gap-3 '>
+                    <img className='h-12 rounded-full object-cover w-12' src="https://i.pinimg.com/originals/8d/fe/2c/8dfe2c8ddc4b7df7bbf77b01c365974d.jpg" alt="" />
+                    <h2 className='text-lg font-semibold'>Eloni Muski</h2>
+                </div>
+                <h5 className='text-lg font-semibold'>2.8 kM</h5>
+            </div>
+
+
+            <div className='flex flex-col gap-2 justify-between items-center '>
+
+                <div className='w-full mt-5'>
+                    <div className='flex items-center gap-5 p-2 border-b-2'>
+                        <i className="text-lg ri-map-pin-2-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>kankariya Talab, Ahemdabad</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-2 border-b-2'>
+                        <i className="text-lg ri-map-pin-user-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>5/22-Z</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Ramnik chok, Ahemdabad</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-2'>
+                        <i className="ri-currency-line"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>₹193.20</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Payment</p>
+                        </div>
+                    </div>
+                </div>
+                <button onClick={() => {
+                   
+
+                }} className='w-full mt-5 bg-green-600 text-white text-semibold p-2 rounded-lg'>Confirm</button>
+
+                <button onClick={() => {
+                    props.setRidePopUpPanel(false)
+
+                }} className='w-full mt-1 bg-gray-300 text-gray-700 text-semibold p-2 rounded-lg'>Ignore</button>
+            </div>
+        </div>
+    );
+};
+
+export default RidePopUp;
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useRef,useState } from 'react';
+import { Link } from 'react-router-dom';
+import CaptainDetails from '../components/CaptainDetails';
+import RidePopUp from '../components/RidePopUp';
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap';
+
 
 const CaptainHome = () => {
+    
+    const [ridePopUpPanel, setRidePopUpPanel] = useState(true) 
+    const ridePopUpPanelRef = useRef(null)
+
+    useGSAP(function () {
+        if (ridePopUpPanel) {
+            gsap.to(ridePopUpPanelRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(ridePopUpPanelRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+    }, [ridePopUpPanel])
+
     return (
-        <div className="captain-home">
-            <h1>Captain Home</h1>
+        <div className='h-screen'>
+            <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+                <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/5/58/Uber_logo_2018.svg" alt="" />
+                <Link to='/captain-login' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full right-2 top-2'>
+                    <i className=" text-lg font-medium ri-logout-box-r-line"></i>
+                </Link>
+            </div>
+            <div className='h-3/5'>
+                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+            </div>
+            <div className='h-2/5 p-6'>
+                <CaptainDetails />
+            </div>
+
+            <div ref={ridePopUpPanelRef} className='fixed w-full translate-y-full z-10 bottom-0  bg-white px-3 py-10 pt-12'>
+                <RidePopUp setRidePopUpPanel={setRidePopUpPanel} />
+            </div>
+
+
         </div>
     );
 };
 
 export default CaptainHome;
+
+
